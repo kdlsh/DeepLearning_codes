f01368d11f9fa7fca08f3b27289746bb1e1f4d07
kdlsh <kdlsh2430@gmail.com>
2019-09-30 17:41:36 +0900

    Update tf_record.py

--- tf2_template/Load_and_Preprocess/tf_record.py
+++ tf2_template/Load_and_Preprocess/tf_record.py
@@ -1,2 +1,11 @@
 # https://www.tensorflow.org/tutorials/load_data/tfrecord
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import numpy as np
+import IPython.display as display
+
+## tf.Example
+# tf.Example is a {"string": tf.train.Feature} mapping
